6bf0c8529d3b90f3f2e3ae9ffcfbd68fc85ded04
Benjamin.Pinseel <br.pinseel@gmail.com>
2019-07-21 16:40:02 +0200

    feature: Added backdrop when clicking mobile-nav item.

--- shared.js
+++ shared.js
@@ -1,11 +1,24 @@
+var backdrop = document.querySelector(".backdrop");
 var mobileNav = document.querySelector(".nav-mobile");
 var headerButton = document.querySelector(".header-button");
 
-headerButton.addEventListener("click", function() {
+headerButton.addEventListener("click", function () {
     if (mobileNav.classList.contains("open")) {
-        mobileNav.classList.remove("open")
-    }
-    else {
-        mobileNav.classList.add("open")
+        closeBackdrop();
+        mobileNav.classList.remove("open");
+    } else {
+        backdrop.style.display = "block";
+        backdrop.classList.add("open");
+        mobileNav.classList.add("open");
     }
 });
+
+backdrop.addEventListener("click", function () {
+    closeBackdrop();
+    mobileNav.classList.remove("open");
+});
+
+function closeBackdrop() {
+    backdrop.classList.remove("open");
+    backdrop.style.display = "none";
+}
